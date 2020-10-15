de78c57681d7f16e63b131df7096c09b7ab6a9d9
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-10-15 14:15:39 -0300

    se esta por terminar de arreglar de UI antes de pasar a la version responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,83 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import "../assets/styles/components/Footer.scss";
+import Logo from "../assets/static/images/img/Logo-Pufi-black.svg";
+import QR from "../assets/static/images/img/QR.png";
+import Shield from "../assets/static/images/img/Shield.png";
+import Facebook from "../assets/static/images/img/Facebook.svg";
+import Instagram from "../assets/static/images/img/Instagram.svg";
+import Twitter from "../assets/static/images/img/Twitter.svg";
+import LogoBrandLive from "../assets/static/images/img/Logo-Brand-Live.svg";
 
 const year = new Date().getFullYear();
 
-const Footer = (props) => {
+const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer_gradient"></div>
       <div className="footer_inside">
-        <div>
-          <img src="" alt="Logo de PUFI" />
+        <div className="footer_inside_logo">
+          <img src={Logo} alt="Logo de PUFI" />
         </div>
         <div>
           <ul>
-            <li>PUFI RAIN</li>
-            <li>PUFI PUFF</li>
-            <li>PUFI CART</li>
-            <li>PUFI NAP</li>
+            <li>
+              <p> PUFI RAIN</p>
+            </li>
+            <li>
+              <p> PUFI PUFF</p>
+            </li>
+            <li>
+              <p> PUFI CART</p>
+            </li>
+            <li>
+              <p> PUFI NAP</p>
+            </li>
           </ul>
         </div>
         <div>
           <ul>
-            <li>CONTACTO</li>
-            <li>AYUDA</li>
-            <li>CÓMO COMPRAR</li>
-            <li>TÉRMINOS & CONDICIONES</li>
+            <li>
+              <p> CONTACTO</p>
+            </li>
+            <li>
+              <p> AYUDA</p>
+            </li>
+            <li>
+              <p> CÓMO COMPRAR</p>
+            </li>
+            <li>
+              <p> TÉRMINOS & CONDICIONES</p>
+            </li>
           </ul>
         </div>
-        <div>
+        <div className="footer_inside_security">
           <h5>COMPRA 100% SEGURA</h5>
           <div>
-            <img src="" alt="Codigo QR" />
-            <img src="" alt="Ícono de seguridad" />
+            <img src={QR} alt="Codigo QR" />
+            <img
+              className="footer_inside_security_shield"
+              src={Shield}
+              alt="Ícono de seguridad"
+            />
             <p>COMPRÁ CON LA GARANTÍA DE PUFI</p>
           </div>
         </div>
-        <div>
+        <div className="footer_inside_socials">
           <p>SEGUINOS EN</p>
-          <img src="" alt="Ícono de Facebook" />
-          <img src="" alt="Ícono de Twitter" />
-          <img src="" alt="Ícono de Instagram" />
+          <div>
+            <img src={Facebook} alt="Ícono de Facebook" />
+            <img src={Twitter} alt="Ícono de Twitter" />
+            <img src={Instagram} alt="Ícono de Instagram" />
+          </div>
         </div>
       </div>
       <div className="footer_under_data">
         <div>
-          <p> PUFI Copyright 2017 - Todos los derechos reservados</p>
+          <p> PUFI Copyright {year} - Todos los derechos reservados</p>
         </div>
         <div>
-          <img src="" alt="Logo de BRAND LIVE" />
+          <img src={LogoBrandLive} alt="Logo de BRAND LIVE" />
         </div>
       </div>
     </div>
