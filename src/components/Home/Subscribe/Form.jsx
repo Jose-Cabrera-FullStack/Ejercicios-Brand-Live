64cc47bfd9b2fbb1eafb4cd5ab9fe9287813fd84
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-10-15 12:50:46 -0300

    se va a terminar el footer

--- src/components/Home/Subscribe/Form.jsx
+++ src/components/Home/Subscribe/Form.jsx
@@ -1,14 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import "../../../assets/styles/components/Subscribe.scss";
 import Arrow from "../../../assets/static/images/img/arrow-right.svg";
 
 const Form = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  const onSubmit = () => {
+    props.loginUser(form);
+  };
+
   return (
-    <form action="" className="subscribe_form">
-      <input placeholder="ingresa tu mail" type="text" />
-      <img src={Arrow} alt="Flecha a la derecha" />
-    </form>
+    <>
+      <form className="subscribe_form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="email"
+          placeholder="ingresa tu mail"
+          type="text"
+          ref={register({
+            required: {
+              value: true,
+              message: "Este campo es requerido",
+            },
+            pattern: {
+              value: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+              message: "Debe ser un email",
+            },
+            maxLength: {
+              value: 50,
+              message: "No puede tener mas de 50 caracteres",
+            },
+            minLength: {
+              value: 5,
+              message: "No puede tener menos de 5 caracteres",
+            },
+          })}
+        />
+        <img src={Arrow} alt="Flecha a la derecha" />
+      </form>
+      <div className="subscribe">
+        <p>{errors.email && errors.email.message}</p>
+      </div>
+    </>
   );
 };
 
